e36ddce32320114f5169a64df96d5e03c25ccdd8
daxoliveira <diogenes.oliveira@rxpro.com.br>
2021-06-25 17:16:18 -0300

    create a login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Admin, Resource } from 'react-admin';
-import { Dashboard } from "./views/Dashboard";
+import Dashboard from './views/Dashboard';
+import authProvider from './authProvider';
+
 import { PostList } from './components/PostList';
 import { PostCreate } from './components/PostCreate';
 import { PostEdit } from './components/PostEdit';
@@ -13,7 +15,7 @@ import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
