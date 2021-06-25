213b92155ba06865a73fa6b519ec1b4103b381d5
daxoliveira <diogenes.oliveira@rxpro.com.br>
2021-06-25 17:16:57 -0300

    fix dashboard import

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { Card, CardContent, CardHeader } from '@material-ui/core';
 
-export const Dashboard = () => (
+const Dashboard = () => (
   <Card>
     <CardHeader title="Welcome to the Administration" />
     <CardContent>Lorem ipsum sic dolor amet...</CardContent>
   </Card>
 );
+
+export default Dashboard;
