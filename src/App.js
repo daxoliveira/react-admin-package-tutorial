06c68202626ff8a7f0e55bb6bcd91a2d4c8af212
daxoliveira <diogenes.oliveira@rxpro.com.br>
2021-06-25 16:31:27 -0300

    implement material-ui icons

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import { PostCreate } from './components/PostCreate';
 import { PostEdit } from './components/PostEdit';
 import { UserList } from './components/Users';
 
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-    <Resource name="users" list={UserList} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
 
