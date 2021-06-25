32e8a477606d18fc687a83c2dd8fa1e8dbfb5374
daxoliveira <diogenes.oliveira@rxpro.com.br>
2021-06-25 15:28:31 -0300

    display title instead of id at page top

--- src/posts.js
+++ src/posts.js
@@ -26,27 +26,30 @@ export const PostList = props => (
   </List>
 );
 
+const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
 export const PostEdit = props => (
-  <Edit {...props}>
-      <SimpleForm>
-        <TextInput disabled source="id" />
-        <ReferenceInput source="userId" reference="users">
-          <SelectInput optionText="name" />
-        </ReferenceInput>
-        <TextInput source="title" />
-        <TextInput multiline source="body" />
-      </SimpleForm>
+  <Edit title={<PostTitle />} {...props}>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
   </Edit>
 );
 
 export const PostCreate = props => (
   <Create {...props}>
-      <SimpleForm>
-        <ReferenceInput source="userId" reference="users">
-          <SelectInput optionText="name" />
-        </ReferenceInput>
-        <TextInput source="title" />
-        <TextInput multiline source="body" />
-      </SimpleForm>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
   </Create>
 );
