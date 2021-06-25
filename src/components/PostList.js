74435dd9738b4e23ee8d617732206b0497c23c2b
daxoliveira <diogenes.oliveira@rxpro.com.br>
2021-06-25 17:44:55 -0300

    add useMediaQuery for mobile display

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,56 +1,30 @@
 import * as React from "react";
 import { PostFilter } from './PostFilter';
-import { 
-  List, 
-  Datagrid, 
-  TextField, 
-  ReferenceField, 
-  EditButton,
-  Edit,
-  Create,
-  SimpleForm,
-  ReferenceInput,
-  SelectInput,
-  TextInput,
-} from 'react-admin';
+import { useMediaQuery } from '@material-ui/core';
+import { List, SimpleList, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
 
-export const PostList = props => (
-  <List filters={<PostFilter />} {...props}>
-    <Datagrid>
-      <TextField source="id" />
-      <ReferenceField source="userId" reference="users">
-        <TextField source="name" />
-      </ReferenceField>
-      <TextField source="title" />
-      <EditButton />
-    </Datagrid>
-  </List>
-);
-
-const PostTitle = ({ record }) => {
-  return <span>Post {record ? `"${record.title}"` : ''}</span>;
-};
-export const PostEdit = props => (
-  <Edit title={<PostTitle />} {...props}>
-    <SimpleForm>
-      <TextInput disabled source="id" />
-      <ReferenceInput source="userId" reference="users">
-        <SelectInput optionText="name" />
-      </ReferenceInput>
-      <TextInput source="title" />
-      <TextInput multiline source="body" />
-    </SimpleForm>
-  </Edit>
-);
-
-export const PostCreate = props => (
-  <Create {...props}>
-    <SimpleForm>
-      <ReferenceInput source="userId" reference="users">
-        <SelectInput optionText="name" />
-      </ReferenceInput>
-      <TextInput source="title" />
-      <TextInput multiline source="body" />
-    </SimpleForm>
-  </Create>
-);
+export const PostList = props => {
+  const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+  return (
+    <List filters={<PostFilter />} {...props}>
+      {
+        isSmall ? (
+          <SimpleList 
+            primaryText={record => record.title}
+          />
+        )
+        :
+        (
+          <Datagrid>
+            <TextField source="id" />
+            <ReferenceField source="userId" reference="users">
+              <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+            <EditButton />
+          </Datagrid>
+        )
+      }
+    </List>
+  );
+}
